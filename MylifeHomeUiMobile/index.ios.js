bff9080019cbdca91c5d49e4ddf3e978273f4bd2
Vincent Trumpff <vincent@stoic.com>
2017-02-20 18:10:50 +0100

    init + socket.io

---
+++ MylifeHomeUiMobile/index.ios.js
@@ -0,0 +1,64 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import io from 'socket.io-client';
+
+
+const socket = io('http://mylife-home-ui.apps.mti-team2.dyndns.org');
+
+console.log('pate');
+
+socket.on('connect', function(){ console.log('connect'); });
+socket.on('event', function(data){ console.log('data', data); });
+socket.on('disconnect', function(){ console.log('disconnect'); });
+
+export default class MylifeHomeUiMobile extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('MylifeHomeUiMobile', () => MylifeHomeUiMobile);
